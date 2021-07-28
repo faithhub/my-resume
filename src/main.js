37cb7e7e12863c4162a6a484cf3130fa7998c9aa
faithdinno <adebayooluwadara@gmail.com>
2021-07-28 22:21:10 +0100

    updates

--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './components/router/index'
+import VueAnalytics from 'vue-analytics';
 
 // import "bootstrap/dist/css/bootstrap.css";
 // import "bootstrap-vue/dist/bootstrap-vue.css";
 
 // Vue.use(BootstrapVue);
+
+// Configuration VueAnalytics
+Vue.use(VueAnalytics, {
+    id: 'G-V1C2TTDB2L',
+    router
+});
 
 Vue.config.productionTip = false
 
